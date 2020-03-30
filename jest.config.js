4d6115cdbbf6929ef70b3f26a3b8021d0c57fc3d
Kenichi Saito <hajime@studiohff.net>
2020-03-31 01:33:09 +0900

    add jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    moduleFileExtensions: [
+        "js",
+        "ts",
+    ],
+    collectCoverageFrom: [
+        "src/**/*.ts",
+    ],
+};
