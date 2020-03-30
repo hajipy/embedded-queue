a81abdbd2f0afc3baa34562c074bfbe10bb48a46
Kenichi Saito <hajime@studiohff.net>
2020-03-31 01:14:03 +0900

    add eslint-plugin-jest 23.8.2

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,13 @@ module.exports = {
     parser: "@typescript-eslint/parser",
     plugins: [
         "@typescript-eslint",
+        "jest",
     ],
     extends: [
         "eslint:recommended",
         "plugin:@typescript-eslint/eslint-recommended",
         "plugin:@typescript-eslint/recommended",
+        "plugin:jest/recommended",
+        "plugin:jest/style",
     ],
 };
