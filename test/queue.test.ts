3906f3c79bd1bc6db941b86c27aa8949e54ddc1a
Kenichi Saito <hajime@studiohff.net>
2020-04-11 22:47:20 +0900

    [WIP] implement Queue unit test

--- test/queue.test.ts
+++ test/queue.test.ts
@@ -2,7 +2,6 @@ import uuid from "uuid/v4";
 
 import { Priority, Queue, State } from "../src";
 import { JobRepository, NeDbJob } from "../src/jobRepository";
-import exp = require("constants");
 
 jest.mock("uuid/v4");
 
@@ -194,4 +193,101 @@ describe("findJob", () => {
         expect(mockedRepositoryFindJob).toHaveBeenCalledTimes(1);
         expect(mockedRepositoryFindJob.mock.calls[0][0]).toBe(id);
     });
+});
+
+test("listJobs", async () => {
+    const queue = await Queue.createQueue({
+        inMemoryOnly: true,
+    });
+
+    const nedbJobs: NeDbJob[] = [
+        {
+            _id: "1",
+            type: "type",
+            priority: Priority.NORMAL,
+            data: {
+                a: "aaa",
+                b: 123,
+                c: {
+                    x: true,
+                    y: {
+                        z: true,
+                    },
+                },
+            },
+            createdAt: new Date(2020, 4, 1, 0, 0, 0),
+            updatedAt: new Date(2020, 4, 2, 0, 0, 0),
+            startedAt: new Date(2020, 4, 3, 0, 0, 0),
+            completedAt: new Date(2020, 4, 4, 0, 0, 0),
+            failedAt: new Date(2020, 4, 5, 0, 0, 0),
+            state: State.INACTIVE,
+            duration: 123,
+            progress: 1 / 3,
+            logs: [
+                "First Log",
+                "Second Log",
+                "Third Log",
+            ],
+        },
+        {
+            _id: "2",
+            type: "type",
+            priority: Priority.HIGH,
+            createdAt: new Date(2020, 4, 6, 0, 0, 0),
+            updatedAt: new Date(2020, 4, 7, 0, 0, 0),
+            startedAt: new Date(2020, 4, 8, 0, 0, 0),
+            completedAt: new Date(2020, 4, 9, 0, 0, 0),
+            failedAt: new Date(2020, 4, 10, 0, 0, 0),
+            state: State.ACTIVE,
+            duration: 234,
+            progress: 2 / 3,
+            logs: [
+            ],
+        },
+        {
+            _id: "3",
+            type: "type",
+            priority: Priority.LOW,
+            createdAt: new Date(2020, 4, 11, 0, 0, 0),
+            updatedAt: new Date(2020, 4, 12, 0, 0, 0),
+            startedAt: new Date(2020, 4, 13, 0, 0, 0),
+            completedAt: new Date(2020, 14, 9, 0, 0, 0),
+            failedAt: new Date(2020, 4, 15, 0, 0, 0),
+            state: State.COMPLETE,
+            duration: 345,
+            progress: 3 / 4,
+            logs: [
+            ],
+        },
+    ];
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const repository = (queue as any).repository as JobRepository;
+    const mockedRepositoryListJobs = jest.fn().mockResolvedValue(nedbJobs);
+    repository.listJobs = mockedRepositoryListJobs;
+
+    const state = State.ACTIVE;
+    const jobs = await queue.listJobs(state);
+
+    expect(jobs).toHaveLength(3);
+
+    for (const [index, job] of jobs.entries()) {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        expect((job as any).queue).toBe(queue);
+        expect(job.id).toBe(nedbJobs[index]._id);
+        expect(job.type).toBe(nedbJobs[index].type);
+        expect(job.priority).toBe(nedbJobs[index].priority);
+        expect(job.data).toEqual(nedbJobs[index].data);
+        expect(job.createdAt).toEqual(nedbJobs[index].createdAt);
+        expect(job.updatedAt).toEqual(nedbJobs[index].updatedAt);
+        expect(job.startedAt).toEqual(nedbJobs[index].startedAt);
+        expect(job.completedAt).toEqual(nedbJobs[index].completedAt);
+        expect(job.failedAt).toEqual(nedbJobs[index].failedAt);
+        expect(job.state).toBe(nedbJobs[index].state);
+        expect(job.logs).toEqual(nedbJobs[index].logs);
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        expect((job as any)._saved).toBe(true);
+    }
+    expect(mockedRepositoryListJobs).toHaveBeenCalledTimes(1);
+    expect(mockedRepositoryListJobs.mock.calls[0][0]).toBe(state);
 });
