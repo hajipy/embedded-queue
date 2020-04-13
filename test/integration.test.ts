d39a65c933ee8ffd77c56ae29a90908787126f33
Kenichi Saito <hajime@studiohff.net>
2020-04-14 00:57:21 +0900

    [WIP] implement integration test

--- test/integration.test.ts
+++ test/integration.test.ts
@@ -1,6 +1,6 @@
 import * as util from "util";
 
-import { Event, Job, Queue } from "../src";
+import { Event, Job, Queue, State } from "../src";
 
 const setTimeoutPromise = util.promisify(setTimeout);
 
@@ -33,7 +33,7 @@ test("Basic Usage", async () => {
     const completeHandler = jest.fn();
     queue.on(Event.Complete, completeHandler);
 
-    await queue.createJob({
+    const createdJob = await queue.createJob({
         type: "adder",
         data: { a: 1, b: 2 },
     });
@@ -43,6 +43,28 @@ test("Basic Usage", async () => {
     expect(completeHandler).toHaveBeenCalledTimes(1);
     expect(completeHandler.mock.calls[0][0]).toBeInstanceOf(Job);
     expect(completeHandler.mock.calls[0][1]).toBe(3);
+
+    const completedJob = completeHandler.mock.calls[0][0];
+    expect(completedJob.id).toBe(createdJob.id);
+    const currentTimestamp = new Date();
+    expect(completedJob.startedAt).not.toBeUndefined();
+    if (completedJob.startedAt !== undefined) {
+        expect((currentTimestamp.getTime() - completedJob.startedAt.getTime())).toBeLessThan(1000);
+    }
+    expect(completedJob.completedAt).not.toBeUndefined();
+    if (completedJob.completedAt !== undefined) {
+        expect((currentTimestamp.getTime() - completedJob.completedAt.getTime())).toBeLessThan(1000);
+    }
+    expect(completedJob.duration).not.toBeUndefined();
+    if (
+        completedJob.duration !== undefined &&
+        completedJob.startedAt !== undefined &&
+        completedJob.completedAt !== undefined
+    ) {
+        expect(completedJob.duration).toBe(completedJob.completedAt.getTime() - completedJob.startedAt.getTime());
+    }
+    expect(completedJob.failedAt).toBeUndefined();
+    expect(completedJob.state).toBe(State.COMPLETE);
 
     // shutdown queue
     setTimeout(async () => { await queue.shutdown(100); }, 1);
