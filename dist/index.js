d93d5ebb3cfe0150099b51e25d5721c61726e3b6
Kenichi Saito <hajime@studiohff.net>
2021-04-02 00:59:44 +0900

    Bump typesciprt from 3.9.9 to 4.2.3

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.Event = exports.State = exports.priorityToString = exports.Priority = exports.Job = exports.Worker = exports.Queue = void 0;
 var queue_1 = require("./queue");
 Object.defineProperty(exports, "Queue", { enumerable: true, get: function () { return queue_1.Queue; } });
 var worker_1 = require("./worker");
